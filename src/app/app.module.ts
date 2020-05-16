b6142d4df5117fa0edd1299c50b63ac6291dbe19
unknown <doyin315@gmaiil.com>
2020-05-16 21:25:48 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ToastrModule} from 'ngx-toastr';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
+    ToastrModule.forRoot(),
+    TooltipModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
